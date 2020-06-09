0f3e34a0721c4d733119044136ddaf6e878c132c
sagan <sagan@syneforge.com>
2020-06-09 18:07:30 +0300

    add plugins

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './module'
+console.log('working!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,40 @@
-console.log('test');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: './index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+
+    },
+    resolve: {
+       extensions: ['.js'],
+        alias: {
+           '@': path.resolve(__dirname, 'src'),
+           '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        }),
+    ],
+}
