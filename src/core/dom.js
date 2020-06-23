c3fb0789573c8d613abab71aed684481d21fb085
sagan <sagan@syneforge.com>
2020-06-23 17:14:44 +0300

    add Class to DOM

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,11 +1,39 @@
 class Dom {
-  constructor() {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector
+  }
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+  clear() {
+    this.html('');
+    return this;
+  }
+  on() {
 
+  }
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+    return this;
   }
 }
 
-export function $() {
-  return new Dom();
+export function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
@@ -13,5 +41,5 @@ $.create = (tagName, classes = '') => {
   if (classes) {
     el.classList.add(classes);
   }
-  return el;
+  return $(el);
 }
