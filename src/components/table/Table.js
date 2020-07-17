24b26bf188ad60d04b60c41e6f4b1550bd510594
sagan <sagan@syneforge.com>
2020-07-17 17:23:53 +0300

    create dynamic table template

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,59 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'table';
+
   toHTML() {
-    return `
-    <div class="table__row">
-        <div class="table__row-info"></div>
-        <div class="table__row-data">
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-            <div class="column">D</div>
-            <div class="column">E</div>
-        </div>
-    </div>
-    <div class="table__row">
-        <div class="table__row-info">1</div>
-        <div class="table__row-data">
-            <div class="cell cell--selected" contenteditable="true">A1</div>
-            <div class="cell" contenteditable="true">B1</div>
-            <div class="cell" contenteditable="true">C1</div>
-            <div class="cell" contenteditable="true">D1</div>
-            <div class="cell" contenteditable="true">E1</div>
-        </div>
-    </div>
-    <div class="table__row">
-        <div class="table__row-info">2</div>
-        <div class="table__row-data">
-            <div class="cell" contenteditable="true">A2</div>
-            <div class="cell" contenteditable="true">B2</div>
-            <div class="cell" contenteditable="true">C2</div>
-            <div class="cell" contenteditable="true">D2</div>
-            <div class="cell" contenteditable="true">E2</div>
-        </div>
-    </div>
-    <div class="table__row">
-        <div class="table__row-info">3</div>
-        <div class="table__row-data">
-            <div class="cell" contenteditable="true">A3</div>
-            <div class="cell" contenteditable="true">B3</div>
-            <div class="cell" contenteditable="true">C3</div>
-            <div class="cell" contenteditable="true">D3</div>
-            <div class="cell" contenteditable="true">E3</div>
-        </div>
-    </div>
-    <div class="table__row">
-        <div class="table__row-info">4</div>
-        <div class="table__row-data">
-            <div class="cell" contenteditable="true">A4</div>
-            <div class="cell" contenteditable="true">B4</div>
-            <div class="cell" contenteditable="true">C4</div>
-            <div class="cell" contenteditable="true">D4</div>
-            <div class="cell" contenteditable="true">E4</div>
-        </div>
-    </div>
-    `;
+    return createTable();
   }
 }
---
+++ src/components/table/table.template.js
@@ -0,0 +1,47 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+function createRow(index, content) {
+  return `
+    <div class="table__row">
+        <div class="table__row-info">${index ? index : ''}</div>
+        <div class="table__row-data">${content}</div>
+    </div>
+  `
+}
+
+function toColumn(col) {
+  return `<div class="column">${col}</div>`
+}
+
+function toCell() {
+  return `<div class="cell" contenteditable></div>`
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rowsCount = 25) {
+  const colsCount = CODES.Z - CODES.A + 1;
+  const rows = [];
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('');
+
+  rows.push(createRow(null, cols));
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
+    rows.push(createRow(i + 1, cells));
+  }
+
+  return rows.join('');
+}
